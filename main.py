782e72dbb0a6a589a13ec92e249a496477caa46d
Tom Fyfe <tomfyfe85@gmail.com>
2024-08-14 11:51:11 +0100

    added all_tasks dict to app.get("/") and added corrs

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    tasks = [
+        {id: "1", "name": "Learn JavaScript", "priority": "five"},
+        {id: "2", "name": "Deploy app", "priority": "five"},
+        {id: "3", "name": "Update project dependencies", "priority": "three"},
+        {id: "4", "name": "Write docs", "priority": "two"},
+        {id: "5", "name": "Refactor app.js", "priority": "one"},
+    ]
+    return {"all_tasks": tasks}
