25a7c4a15153c5bedf218212e7e5e6a3ed69dc34
Tom Fyfe <tomfyfe85@gmail.com>
2024-10-08 20:44:34 +0100

    crated User schema and foreign key for tasks

--- schemas.py
+++ schemas.py
@@ -2,8 +2,6 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-
-
 class TaskBase(BaseModel):
     name: str
     priority: str
@@ -14,6 +12,7 @@ class TaskCreate(TaskBase):
 #Task inherits form TaskBase 
 class Task(TaskBase):
     id: int
+    user_id: int
 
 class TaskUpdate(BaseModel):
     name: Optional[str] = Field(default=None)
@@ -22,3 +21,18 @@ class TaskUpdate(BaseModel):
     class ConfigDict:
         orm_mode = True
 
+#
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    tasks: list[Task] = []
+
+    class ConfigDict:
+        orm_mode = True
