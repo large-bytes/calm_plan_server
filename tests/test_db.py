f22ec173549b767ee7fd4a1bdef6eaac5d138752
Tom FYFE <tomfyfe85@gmail.com>
2024-10-31 07:24:51 +0000

    first test passes

--- tests/test_db.py
+++ tests/test_db.py
@@ -6,8 +6,7 @@ import dotenv
 import os
 dotenv.load_dotenv()
 
-# TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
-TEST_DATABASE_URL = 'postgresql://tomfyfe@5432/calm_plan_test/'
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 engine = create_engine(
     TEST_DATABASE_URL
 )
