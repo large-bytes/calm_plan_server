f1ea2b171bc7d384e0bdb1e6f0c70e6af7ae0189
Tom FYFE <tomfyfe85@gmail.com>
2024-11-06 19:18:54 +0000

    added router for tasks, all but one test passes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
+from . tasks_router import router
 from . src import schemas
 from . src.models import Base
 from . src.database import get_db, engine
@@ -24,44 +25,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/tasks")
-async def read_all_tasks(db: Session = Depends(get_db)):
-    results = db.query(Task).all()
-    return results
+app.include_router(router)
 
-@app.post("/tasks")
-async def add_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    new_task = Task(name=task.name, priority=task.priority)
-    db.add(new_task)
-    db.commit()
-    db.refresh(new_task)
-    return new_task
-
-@app.get("/tasks/{id}")
-async def get_task_by_id(id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == id).first()
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return task
-
-@app.delete("/tasks/{id}")
-async def delete_task_by_id(id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == id).first()
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    db.delete(task)
-    db.commit()
-    return {"ok": True}
-
-@app.patch("/tasks/{id}")
-async def update_task_by_id(id: int, updated_task: schemas.TaskUpdate, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == id).first()
-    task_data = updated_task.model_dump(exclude_unset=True)
-    for k, v in task_data.items():
-        setattr(task, k, v)
-    db.commit()
-    db.refresh(task)
-    return task
+# @app.get("/tasks")
+# async def read_all_tasks(db: Session = Depends(get_db)):
+#     results = db.query(Task).all()
+#     return results
+#
+# @app.post("/tasks")
+# async def add_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+#     new_task = Task(name=task.name, priority=task.priority)
+#     db.add(new_task)
+#     db.commit()
+#     db.refresh(new_task)
+#     return new_task
+#
+# @app.get("/tasks/{id}")
+# async def get_task_by_id(id: int, db: Session = Depends(get_db)):
+#     task = db.query(Task).filter(Task.id == id).first()
+#     if not task:
+#         raise HTTPException(status_code=404, detail="Task not found")
+#     return task
+#
+# @app.delete("/tasks/{id}")
+# async def delete_task_by_id(id: int, db: Session = Depends(get_db)):
+#     task = db.query(Task).filter(Task.id == id).first()
+#     if not task:
+#         raise HTTPException(status_code=404, detail="Task not found")
+#     db.delete(task)
+#     db.commit()
+#     return {"ok": True}
+#
+# @app.patch("/tasks/{id}")
+# async def update_task_by_id(id: int, updated_task: schemas.TaskUpdate, db: Session = Depends(get_db)):
+#     task = db.query(Task).filter(Task.id == id).first()
+#     task_data = updated_task.model_dump(exclude_unset=True)
+#     for k, v in task_data.items():
+#         setattr(task, k, v)
+#     db.commit()
+#     db.refresh(task)
+#     return task
 
 @app.get("/users")
 async def read_all_users(db: Session = Depends(get_db)):
---
+++ tasks_router.py
@@ -0,0 +1,53 @@
+from http.client import HTTPException
+from sys import prefix
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from . src import schemas
+from . src.database import get_db
+from . src.models import Task
+
+router = APIRouter(
+    prefix = '/tasks',
+    tags = ['crud_tasks']
+)
+
+@router.get("")
+async def read_all_tasks(db: Session = Depends(get_db)):
+    results = db.query(Task).all()
+    return results
+
+@router.post("")
+async def add_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    new_task = Task(name=task.name, priority=task.priority)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+@router.get("/{id}")
+async def get_task_by_id(id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+@router.delete("/{id}")
+async def delete_task_by_id(id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task)
+    db.commit()
+    return {"ok": True}
+
+@router.patch("/{id}")
+async def update_task_by_id(id: int, updated_task: schemas.TaskUpdate, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+    task_data = updated_task.model_dump(exclude_unset=True)
+    for k, v in task_data.items():
+        setattr(task, k, v)
+    db.commit()
+    db.refresh(task)
+    return task
