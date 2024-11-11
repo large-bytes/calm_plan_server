54700f8c6004b6643c38085a51adbebc27edc89f
Tom FYFE <tomfyfe85@gmail.com>
2024-11-11 20:50:17 +0000

    #all_all_users passes #test_db_is_populated_with_test_data

--- tests/tests_for_users/test_users.py
+++ tests/tests_for_users/test_users.py
@@ -1,4 +1,4 @@
-def test_users_returns_client_gives_200(test_db_client, populate_test_db):
+def test_users_returns_client_gives_200(test_db_client):
     response = test_db_client.get("/users")
     assert response.status_code == 200
 
@@ -6,3 +6,6 @@ def test_returns_empty_list_for_response(test_db_client):
     response = test_db_client.get("/users")
     assert response.json() == []
 
+def test_db_is_populated_with_test_data(test_db_client, populate_test_db):
+    response = test_db_client.get("/users")
+    assert response.json() == [{'username': 'User1', 'id': 1, 'email': "email@email.com", 'password': "12345678",  'is_active': True}]
