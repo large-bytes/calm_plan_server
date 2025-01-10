4821e6146db4e3a05915b94c137d39e59a093efc
tomfyfe <tomfyfe85@gmail.com>
2025-01-10 09:21:26 +0000

    migrated changes to supabase

--- src/models.py
+++ src/models.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String(225))
     disabled = Column(Boolean, default=False)
 
     tasks = relationship("Task", back_populates="user")
