f2113b77fce3bc0d69fb195194bbe4a8be5f8b73
tomfyfe <tomfyfe85@gmail.com>
2025-04-19 00:18:30 +0100

    create_user_model on post from auth

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.schemas import CreateUserRequest
+from src.models import User
 
 router = APIRouter()
 
@@ -7,10 +8,16 @@ router = APIRouter()
 
 
 @router.post("/auth/")
-async def create_user():
-	return {'user': 'authenticated'}
+async def create_user(create_user_request: CreateUserRequest):
+	create_user_model = User(
+		username=create_user_request.username,
+		email=create_user_request.email,
+		role=create_user_request.role,
+		hashed_password=create_user_request.password,
+		is_active=True
+	)
 
-
+	return create_user_model
 
 
 
