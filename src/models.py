e2ce1e6d12fd777bc7b90e6b039453d2fcf344ef
tomfyfe <tomfyfe85@gmail.com>
2025-04-18 14:37:50 +0100

    changed owners to users in models.py

--- src/models.py
+++ src/models.py
@@ -3,15 +3,16 @@ from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
-# #USER MODEL CLASS -
+# #USERS MODEL CLASS -
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, unique=True)
     email = Column(String, unique=True, nullable=False)
     hashed_password = Column(String(225))
-    disabled = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
     tasks = relationship("Task", back_populates="user")
 
@@ -19,16 +20,17 @@ class Users(Base):
         return f"id:{self.id}, username: {self.username}"
 
 
-# TASK MODEL CLASS -
+# TASKS MODEL CLASS -
 class Tasks(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, index=True)
     priority = Column(String, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="tasks")
+    users = relationship("User", back_populates="tasks")
 
     def __repr__(self):
         return f"id:{self.id}, name: {self.username}, priority: {self.prority}"
