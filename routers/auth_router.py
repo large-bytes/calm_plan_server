af6ee81854e291e4cc759921d2b9a1666916e8b1
tomfyfe <tomfyfe85@gmail.com>
2025-04-22 20:16:44 +0100

    users password is hashed

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter
+from typing_extensions import deprecated
+
 from src.schemas import CreateUserRequest
 from src.models import User
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-
-
-@router.post("/auth/")
+@router.post("/auth")
 async def create_user(create_user_request: CreateUserRequest):
 	create_user_model = User(
 		username=create_user_request.username,
 		email=create_user_request.email,
 		role=create_user_request.role,
-		hashed_password=create_user_request.password,
+		hashed_password=bcrypt_context.hash(create_user_request.password),
 		is_active=True
 	)
 
