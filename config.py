69f2f3667ed7fd15a92014a325c35096bf63ab8a
tomfyfe <tomfyfe85@gmail.com>
2025-05-01 11:35:59 +0100

    updated settings to config.py

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ load_dotenv(dotenv_path=env_file)
 
 # Configuration class
 class Settings:
-	ENVIRONMENT: str = os.getenv("ENVIRONMENT", "development")
-	SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-	SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
-
-# Add other configuration settings as needed
-
+    ENVIRONMENT: str = os.getenv("ENVIRONMENT", "development")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    TEST_DATABASE_URL: str = os.getenv("TEST_DATABASE_URL")
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
 
 # Create settings instance
 settings = Settings()
