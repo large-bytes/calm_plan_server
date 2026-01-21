bb7f27c9549735e6e31363609ed93a86d54e9f7e
tomfyfe <tomfyfe85@gmail.com>
2026-01-21 17:31:10 +0000

    add password_utils

--- src/jwt_utils.py
+++ src/jwt_utils.py
@@ -1,16 +1,19 @@
 from jose import jwt
 from datetime import datetime, timedelta, timezone
+import os 
 
-SECRET_KEY = "this-is-a-secret-key"
+SECRET_KEY = os.getenv("SECRET_KEY") 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
     return encoded_jwt
+
+def decode_access_token(encoded_jwt: str) -> dict:
+    payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=[ALGORITHM])
+    return payload
 
---
+++ src/password_utils.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+def hash_password(password: str) -> str:
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password_str: str) -> bool:
+    password_byte_enc = plain_password.encode('utf-8')
+    hashed_password_enc = hashed_password_str.encode('utf-8')
+    return bcrypt.checkpw(password= password_byte_enc, hashed_password= hashed_password_enc)
