1500a285dea1b5d73d71dec8f36f778357715991
tomfyfe <tomfyfe85@gmail.com>
2026-01-24 15:06:59 +0000

    chor tidy up main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-import jwt
 from jose.constants import ALGORITHMS
-from jwt import InvalidTokenError
+from fastapi import FastAPI
 
-from src.database import get_db
-
-from src.schemas import UserInDB, TokenData, Token
-from src.models import User
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, status
-from typing import Annotated
-from sqlalchemy.orm import Session
-from datetime import datetime, timedelta, timezone
-
-from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
-import os
-
-from passlib.context import CryptContext
-
 load_dotenv()
 from routers import auth_router, tasks_router, users_router
 
@@ -41,8 +25,6 @@ app.add_middleware(
 
 app.include_router(tasks_router.router)
 app.include_router(users_router.router)
-
-# #auth router
 app.include_router(auth_router.router)
 
 
